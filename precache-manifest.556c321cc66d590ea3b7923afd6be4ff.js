0f0980802e2c5f22c84fe92eabc72ff91ab1ebca
ell-hol <yacine.zahidi@gmail.com>
2022-03-11 11:47:50 +0100

    Updates

---
+++ precache-manifest.556c321cc66d590ea3b7923afd6be4ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dbf2c1dc14a400aed52",
+    "url": "/regs-ghpages/static/css/main.9b35af86.chunk.css"
+  },
+  {
+    "revision": "9dbf2c1dc14a400aed52",
+    "url": "/regs-ghpages/static/js/main.9dbf2c1d.chunk.js"
+  },
+  {
+    "revision": "ee49389fac822782fe3d",
+    "url": "/regs-ghpages/static/js/1.ee49389f.chunk.js"
+  },
+  {
+    "revision": "c917790327182777de6e",
+    "url": "/regs-ghpages/static/js/runtime~main.c9177903.js"
+  },
+  {
+    "revision": "76ca14a2ed956e610e483014ae877d0d",
+    "url": "/regs-ghpages/index.html"
+  }
+];
